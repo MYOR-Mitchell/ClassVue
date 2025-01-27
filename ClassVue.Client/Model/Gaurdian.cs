8be74e19091187ff79c65c02edd73f75acf45234
MYOR-Mitchell <Martin.Mitchell@MyYahoo.com>
2025-01-26 23:12:59 -0500

    Initial commit

---
+++ ClassVue.Client/Model/Gaurdian.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassVue.Client.Model
+{
+    public class Gaurdian
+    {
+        public string Relation { get; set; }
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ ClassVue.Client/Model/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassVue.Client.Model
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly BirthDay { get; set; }
+        public string Grades { get; set; }
+        public string Email { get; set; }
+    }
+}
